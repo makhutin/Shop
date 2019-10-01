34af7582892555ade13681baf833bea2b3cacdaf
Mahutin Aleksej <mahutin@me.com>
2019-10-01 11:10:49 +0300

    Initial Commit

---
+++ Shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shop
+//
+//  Created by Mahutin Aleksei on 01/10/2019.
+//  Copyright © 2019 Mahutin Aleksei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
